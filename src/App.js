d5a223faee379427e34841aaa6d333cd662e5d14
Omar Barragan <flaky_11@hotmail.com>
2022-05-16 00:28:22 -0500

    Ruta privada

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RecoverPassword from './components/RecoverPassword/RecoverPassword';
 import EmailSent from './components/RecoverPassword/EmailSent';
 import Dashboard from './components/Dashboard/Dashboard';
+import { useState } from 'react';
 
 function App() {
+  const [token, setToken] = useState();
+
   return (
     <BrowserRouter>
       <div className='App'>
         <Routes>
-          <Route path='/' element={<Login />} />
+          {token ? (
+            <Route path='/mailsent' element={<Dashboard />} />
+          ) : (
+            <Route path='/' element={<Login setToken={setToken} />} />
+          )}
           <Route path='/recuperate' element={<RecoverPassword />} />
           <Route path='/mailsent' element={<EmailSent />} />
-          <Route path='/mailsent' element={<Dashboard />} />
         </Routes>
       </div>
     </BrowserRouter>
