a792af4adf64acb4cb055f3dfea6bd5e52b769e0
Omar Barragan <flaky_11@hotmail.com>
2022-05-16 10:54:16 -0500

    corriguiendo errores

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login />} />
+          <Route path='/login' element={<Login />} />
           <Route path='/dash' element={<Dashboard />} />
           <Route path='/recuperate' element={<RecoverPassword />} />
           <Route path='/mailsent' element={<EmailSent />} />
