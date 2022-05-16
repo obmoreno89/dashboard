591210ea08e0c849b782cdd1dc7d7d1b52791a36
Omar Barragan <flaky_11@hotmail.com>
2022-05-16 10:34:57 -0500

    corriguiendo errores

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RecoverPassword from './components/RecoverPassword/RecoverPassword';
 import EmailSent from './components/RecoverPassword/EmailSent';
 import Dashboard from './components/Dashboard/Dashboard';
-import { useState } from 'react';
 
 function App() {
   return (
     <BrowserRouter>
       <div className='App'>
         <Routes>
-          <Route path='/dash' element={<Dashboard />} />
           <Route path='/' element={<Login />} />
+          <Route path='/dash' element={<Dashboard />} />
           <Route path='/recuperate' element={<RecoverPassword />} />
           <Route path='/mailsent' element={<EmailSent />} />
         </Routes>
