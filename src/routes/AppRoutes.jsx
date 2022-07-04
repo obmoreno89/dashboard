89803b0f90e8a7469b9ecdecf9bb76c09a059bbe
Omar Barragan <flaky_11@hotmail.com>
2022-07-04 12:45:58 -0500

    pruebas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -20,7 +20,7 @@ import ThanksPage from '../pages/ThanksPage/ThanksPage';
 
 function AppRoutes() {
   return (
-    <BrowserRouter basename='/supplier-dashboard/'>
+    <BrowserRouter basename='/dashboard'>
       {/* Public Routes */}
       <Routes>
         <Route
